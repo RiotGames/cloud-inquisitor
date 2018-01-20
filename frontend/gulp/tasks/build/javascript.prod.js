679273d13de5087cb7553018506a4b414651e54d
Asbjorn Kjaer <akjaer@riotgames.com>
2018-01-19 16:44:10 -0800

    Fix issue between ng-annotate/uglify and new babel

--- frontend/gulp/tasks/build/javascript.prod.js
+++ frontend/gulp/tasks/build/javascript.prod.js
@@ -6,10 +6,8 @@ const gulp      = require('gulp'),
     browserify  = require('browserify'),
     buffer      = require('vinyl-buffer'),
     config      = require('../../config.json'),
-    ngAnnotate  = require('gulp-ng-annotate'),
     source      = require('vinyl-source-stream'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    uglify      = require('gulp-uglify')
+    sourcemaps  = require('gulp-sourcemaps')
 ;
 
 const pkg = require('../../../package.json');
@@ -18,19 +16,21 @@ const deps = Object.keys(pkg.dependencies);
 gulp.task('build.javascript.prod', function () {
     return browserify(config.app)
         .transform("babelify", {
-            presets: [['env', {
-                "targets": {
-                    "chrome": "60"
-                }
-            }]]
+            presets: [
+                ['env', {
+                    "targets": {
+                        "chrome": "60"
+                    }
+                }],
+                "minify"
+            ],
+            plugins: ["angularjs-annotate"]
         })
         .external(deps)
         .bundle()
         .pipe(source('bundle.js'))
         .pipe(buffer())
         .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(ngAnnotate())
-        .pipe(uglify())
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(config.build + '/js'));
 });
