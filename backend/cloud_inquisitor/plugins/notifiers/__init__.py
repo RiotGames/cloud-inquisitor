1381625cb6e61b643de40ebae04332d4bfbce21a
Asbjorn Kjaer <akjaer@riotgames.com>
2018-03-01 12:39:26 -0800

    Adding base class for notifier plugins as well

--- backend/cloud_inquisitor/plugins/notifiers/__init__.py
+++ backend/cloud_inquisitor/plugins/notifiers/__init__.py
@@ -1,16 +1,17 @@
 from abc import abstractmethod, ABC
 
+from cloud_inquisitor.config import dbconfig
 from cloud_inquisitor.plugins import BasePlugin
 
 
 class BaseNotifier(BasePlugin, ABC):
-    @property
-    @abstractmethod
-    def name(self): pass
+    @classmethod
+    def enabled(cls):
+        return dbconfig.get('enabled', cls.ns, False)
 
     @property
     @abstractmethod
-    def enabled(self): pass
+    def name(self): pass
 
     @property
     @abstractmethod
