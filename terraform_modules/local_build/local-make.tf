d163728c00abb9f5828bd7d12e2b1ccc26ac6f9f
Jesse Taylor <jetaylor@riotgames.com>
2020-08-31 16:05:56 -0700

    added build instructions

--- terraform_modules/local_build/local-make.tf
+++ terraform_modules/local_build/local-make.tf
@@ -3,8 +3,13 @@ resource "null_resource" "local_machine" {
     provisioner "local-exec" {
         working_dir = var.working_dir
         command = <<CMD
-    git clone -brance ${var.branch} https://github.com/RiotGames/cloud-inquisitor.git
-    CMD
+            git clone -b ${var.branch} https://github.com/RiotGames/cloud-inquisitor.git &&\
+            cd cloud-inquisitor &&\
+        CMD
+
+        environment = {
+            SETTINGS_FILE = var.settings_file
+        }
     }
 
 }
@@ -19,4 +24,13 @@ variable "working_dir" {
     type = string
     description = "path to clone repo into"
     default = "./"
+}
+
+variable "settings_file" {
+    type = string
+    description = "path to settings file"
+}
+
+output "binary_path" {
+    value = "${var.working_dir}/cloud-inquisitor/builds"
 }
